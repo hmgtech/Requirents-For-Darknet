8d3ae20887e2589cf8934a098f06959ef1ead9e4
Hitesh Gupta <59255201+hmgtech@users.noreply.github.com>
2020-03-27 21:43:03 +0530

    Update generate_train.py

--- generate_train.py
+++ generate_train.py
@@ -3,7 +3,7 @@ import os
 image_files = []
 os.chdir(os.path.join("data", "obj"))
 for filename in os.listdir(os.getcwd()):
-    if filename.endswith(".jpg"):
+    if filename.endswith(".jpg"): ##it might cause error. just replace .jpg with ur image's extension
     	image_files.append("data/obj/" + filename)
 os.chdir("..")
 with open("train.txt", "w") as outfile:
